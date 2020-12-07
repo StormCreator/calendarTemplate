31c2d15351de3f06651f2c8e8f3406fe0a56d8db
MarginToppson <insetml@gmail.com>
2020-12-07 17:59:22 +0200

    table component remove

--- src/components/Calendar/calendar.js
+++ src/components/Calendar/calendar.js
@@ -1,25 +1,32 @@
-import { Table } from "../table";
 import { MonthPicker } from "../monthPicker";
 import { Component } from "../component";
 import { CalendarHead } from "../calendarHead";
-
+import { CalendarBody } from "../calendarBody";
+import { departmentTeams } from "../../data";
 
 
 export class Calendar{
     constructor(departmentTeams) {
         this.currentDate = new Date();
-        this.table = new Table("#app","table","table", this.currentDate);
-        this.calendarHead = new CalendarHead("#app", 'thead', 'calendar-head', this.currentDate);
+        // this.table = new Table("#app","table","table", this.currentDate);
+        this.table = new Component("#app", "table", "table");
+        this.calendarHead = new CalendarHead(this.table.component, 'thead', 'calendar-head', this.currentDate);
         this.monthPicker = new MonthPicker("#app","div","month-picker", this.calendarHead);
     }
     render() {
         this.monthPicker.render();
-        this.calendarHead.setParent(this.table.component);
+        // this.calendarHead.setParent(this.table.component);
         this.calendarHead.render();
         this.calendarHead.prepend(new Component(this.calendarHead.component, 'td', 'add-vacation').component);
         this.table.render();
+                               
 
     }
 
-
+    addTeamBody(){
+        for(let i = 0; i < departmentTeams.teams.length; i++){
+            let teamBody = new CalendarBody(this.table, "tbody", "body", departmentTeams.teams[i], this.currentDate);
+            teamBody.render();
+        }
+    }
 }
